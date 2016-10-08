5bb8cb5d838392999f0588a5250077611a81d55e
ernest <hunjae.lee@gmail.com>
2016-10-08 11:41:43 +0900

    delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-USER_ID=${LOCAL_USER_ID:-0}
-
-echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -g "" gosuuser
-export HOME=/home/gosuuser
-
-exec /usr/local/bin/gosu gosuuser "$@"
